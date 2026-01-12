a81f236117af067c8d9969321e5e14fe1b500220
Dan Valinotti <dvalinotti@icloud.com>
2026-01-11 21:15:47 -0500

    Fix github logo in header

--- src/components/ThemeToggle.tsx
+++ src/components/ThemeToggle.tsx
@@ -12,8 +12,8 @@ export function ThemeToggle() {
       {theme === "dark" ? (
         <svg
           xmlns="http://www.w3.org/2000/svg"
-          width="20"
-          height="20"
+          width="24"
+          height="24"
           viewBox="0 0 24 24"
           fill="none"
           stroke="currentColor"
